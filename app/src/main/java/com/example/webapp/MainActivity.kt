2f53890687ecde7263b18f958fd02c453cb4407b
Ian <baorii@gmail.com>
2026-01-28 12:55:55 +0100

    Primer commit

---
+++ app/src/main/java/com/example/webapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.webapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.webapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // CONFIGURACIÓN NECESARIA
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.settings.domStorageEnabled = true
+        binding.webView.loadUrl("https://cv.chalichen.cat")
+    }
+}
